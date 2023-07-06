a04e6f507da3f779f24c2785005a81ffd6527c52
Angeliki Bakogianni <agelikibakogianni@gmail.com>
2023-07-06 16:23:51 +0200

    Add esling disable next line for unexpected any

--- src/transform/Country.tsx
+++ src/transform/Country.tsx
@@ -1,8 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const sortByCountryName = (a: any, b: any) => {
   return a.name.common.localeCompare(b.name.common);
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const phoneCountryTrans = (data: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return data?.sort(sortByCountryName)?.map((country: any) => {
     const code =
       country.idd.root?.toString() + country.idd?.suffixes?.[0]?.toString();
@@ -15,7 +18,9 @@ export const phoneCountryTrans = (data: any) => {
   });
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getCountryNames = (data: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return data?.sort(sortByCountryName)?.map((country: any) => {
     return {
       name: country.name.common,
@@ -25,9 +30,12 @@ export const getCountryNames = (data: any) => {
   });
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const getCountrySelection = (data: any) => {
   return data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ?.sort(sortByCountryName)?.filter((country: any) => country.ccn3 == 826 || country.ccn3 == 276)
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     .map((country: any) => {
       return {
         imgSrc: country.flags.svg || country.flags.png,
